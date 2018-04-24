f3ce9769d47ae4b435997e8bde1339176cae723d
Michal Fulara <michalfulara@icloud.com>
2018-04-24 13:43:31 +0200

    Added login test

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -6,9 +6,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.sql.Driver;
+
 public class LoginTests {
 
     WebDriver driver = new ChromeDriver();
+    public String UrlBase = "http://parabank.parasoft.com/parabank/index.htm";
+
+
 
     public void waitForPageLoad(WebDriver webDriver) {
         Wait<WebDriver> wait = new WebDriverWait(webDriver, 30);
@@ -18,13 +23,27 @@ public class LoginTests {
     @Test(priority = 1)
 
     public void shouldNotLogin() {
-        String UrlBase = "http://parabank.parasoft.com/parabank/index.htm";
+
         driver.get(UrlBase);
         waitForPageLoad(driver);
 
-        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("xxx");
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("xxx");
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("xx");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("xx57asd86");
         driver.findElement(By.xpath("//input[@type='submit']")).sendKeys(Keys.ENTER);
         Assert.assertTrue(driver.findElement(By.xpath("//h1[text()='Error!']")).isDisplayed());
     }
+
+    @Test(priority = 2)
+
+    public void shouldLogin(){
+        driver.get(UrlBase);
+        waitForPageLoad(driver);
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("misiek201");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("123456789");
+        driver.findElement(By.xpath("//input[@type='submit']")).sendKeys(Keys.ENTER);
+        Assert.assertTrue(driver.findElement(By.xpath("//thead/tr/th[text()='Account']")).isDisplayed());
+
+
+    }
+
 }
