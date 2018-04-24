411621516f44194c8910916f876f1f71764e5986
Michal Fulara <michalfulara@icloud.com>
2018-04-24 17:18:34 +0200

    code refactored in loginTest

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -20,27 +20,30 @@ public class LoginTests {
         wait.until(webDriver1 -> (JavascriptExecutor) webDriver).executeScript("return document.readyState");
     }
 
-    @Test(priority = 1)
-
-    public void shouldNotLogin() {
-
+    public void loginFields(String username, String password){
         driver.get(UrlBase);
         waitForPageLoad(driver);
-
-        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("xx");
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("xx57asd86");
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys(username);
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
         driver.findElement(By.xpath("//input[@type='submit']")).sendKeys(Keys.ENTER);
+    }
+
+    @Test(priority = 1)
+
+    public void shouldNotLoginWithWrongPassword() {
+
+
+        loginFields("misiek201","123123123");
+
         Assert.assertTrue(driver.findElement(By.xpath("//h1[text()='Error!']")).isDisplayed());
     }
 
     @Test(priority = 2)
 
     public void shouldLogin(){
-        driver.get(UrlBase);
-        waitForPageLoad(driver);
-        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("misiek201");
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("123456789");
-        driver.findElement(By.xpath("//input[@type='submit']")).sendKeys(Keys.ENTER);
+
+        loginFields("misiek201","123456789");
+
         Assert.assertTrue(driver.findElement(By.xpath("//thead/tr/th[text()='Account']")).isDisplayed());
 
 
